41325cbae2e2a15e60e8021928d52baaa53b439b
MoaazElneshawy <m.elneshawy@gmail.com>
2021-03-29 10:32:22 +0300

    use layout builder to make sizes dynamic

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,41 +9,46 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            height: 20,
-            child: FittedBox(child: Text('\$${amount.toStringAsFixed(0)}'))),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          width: 10,
-          height: 60,
-          child: Stack(
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                color: Colors.black26,
-                border: Border.all(width: 1, color: Colors.black),
-                borderRadius: BorderRadius.circular(10),
-              )),
-              FractionallySizedBox(
-                heightFactor: spendingPC,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-              )
-            ],
+    return LayoutBuilder(builder: (context, constraints) {
+      return Column(
+        children: [
+          Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(child: Text('\$${amount.toStringAsFixed(0)}'))),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
           ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Text('$lable')
-      ],
-    );
+          Container(
+            width: 10,
+            height: constraints.maxHeight * 0.6,
+            child: Stack(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                  color: Colors.black26,
+                  border: Border.all(width: 1, color: Colors.black),
+                  borderRadius: BorderRadius.circular(10),
+                )),
+                FractionallySizedBox(
+                  heightFactor: spendingPC,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(child: Text(lable)),
+          )
+        ],
+      );
+    });
   }
 }
